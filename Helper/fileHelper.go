445a4bc22ddba6acce8deea9bdb508f9c450d59d
KiranM27 <kiran_bodhapati@mymail.sutd.edu.sg>
2022-03-21 01:07:19 +0800

    writing data to file doesnt work

--- Helper/fileHelper.go
+++ Helper/fileHelper.go
@@ -1,1 +1,30 @@
 package helper
+
+import (
+	"os"
+	"fmt"
+)
+
+func PathExists(path string) (bool, error) {
+    _, err := os.Stat(path)
+    if err == nil { return true, nil }
+    if os.IsNotExist(err) { return false, nil }
+    return false, err
+}
+
+func CreateFolder(path string) {
+	if pathExists, _ := PathExists(path); !pathExists {
+		err := os.Mkdir(path, 0777)
+		if err != nil {
+			fmt.Println("Error while creating ", path, " : ", err)
+		}
+	}
+}
+
+func CreateFile(path string) {
+	fh, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0777)
+	defer fh.Close()
+    if err != nil {
+        fmt.Println(err)
+    }
+}
