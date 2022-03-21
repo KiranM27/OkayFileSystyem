1996fe9629bf310c1efaa8fd2651c67f64a51dce
KiranM27 <kiran_bodhapati@mymail.sutd.edu.sg>
2022-03-21 13:06:54 +0800

    Can pass in the source file name and the destination file name to client

--- Client/client.go
+++ Client/client.go
@@ -42,15 +42,16 @@ func messageHandler(context *gin.Context) {
 }
 
 // Send a request to Master that client wants to append
-func requestMasterAppend(clientPort int, filename string) {
+// The message has the size of the data
+func requestMasterAppend(clientPort int, sourceFilename string, OFSFilename string) {
 	var numChunks uint64
 
-	// Call helper function to read file size
-	fileByteSize := getFileSize(filename)
+	// Call helper function to read source file size
+	sourceFileByteSize := getFileSize(sourceFilename)
 
 	// Check byte size of file, if more than 2.5kb split
-	if fileByteSize > 2500 {
-		numChunks = splitFile(filename)
+	if sourceFileByteSize > 2500 {
+		numChunks = splitFile(sourceFilename)
 	} else {
 		numChunks = 1
 	}
@@ -59,11 +60,12 @@ func requestMasterAppend(clientPort int, filename string) {
 	// If split, for loop to append
 	if numChunks == 1 {
 		message := structs.Message{
-			MessageType: helper.DATA_APPEND,
-			Ports:       []int{clientPort, helper.MASTER_SERVER_PORT}, // 0 is client, 1 is master
-			Pointer:     1,
-			Filename:    filename,
-			PayloadSize: fileByteSize,
+			MessageType:    helper.DATA_APPEND,
+			Ports:          []int{clientPort, helper.MASTER_SERVER_PORT}, // 0 is client, 1 is master
+			Pointer:        1,
+			SourceFilename: sourceFilename,
+			Filename:       OFSFilename, // File name in the Okay File System
+			PayloadSize:    sourceFileByteSize,
 		}
 		// HTTP Request to Master
 		fmt.Println(message)
@@ -72,19 +74,20 @@ func requestMasterAppend(clientPort int, filename string) {
 		fmt.Println("Finished sending append request to Master")
 
 	} else {
-		filePrefix := removeExtension(filename)
+		sourceFilePrefix := removeExtension(sourceFilename)
 		for i := uint64(0); i < numChunks; i++ {
-			smallFileName := filePrefix + strconv.FormatUint(i, 10) + ".txt"
+			smallFileName := sourceFilePrefix + strconv.FormatUint(i, 10) + ".txt"
 			smallFileSize := getFileSize(smallFileName)
 			fmt.Println(smallFileName) // debug
 			fmt.Println(smallFileSize) // debug
 
 			message := structs.Message{
-				MessageType: helper.DATA_APPEND,
-				Ports:       []int{clientPort, helper.MASTER_SERVER_PORT}, // 0 is client, 1 is master
-				Pointer:     1,
-				Filename:    smallFileName,
-				PayloadSize: smallFileSize,
+				MessageType:    helper.DATA_APPEND,
+				Ports:          []int{clientPort, helper.MASTER_SERVER_PORT}, // 0 is client, 1 is master
+				Pointer:        1,
+				SourceFilename: smallFileName,
+				Filename:       OFSFilename, // TODO: This should be the same file name still right
+				PayloadSize:    smallFileSize,
 			}
 			fmt.Println(message)
 			// HTTP Request to Master
@@ -122,7 +125,7 @@ func sendChunkAppend(message structs.Message, tryAgain bool, context *gin.Contex
 
 	// message.Pointer += 1 // increment the pointer first (initial pointer should be 0)
 	message.Forward()
-	message.Payload = readFile(message.Filename)
+	message.Payload = readFile(message.SourceFilename)
 
 	// HTTP Request to Primary Chunk
 	fmt.Println("Sending append request to Primary Chunk Server")
@@ -143,7 +146,7 @@ func sendChunkAppend(message structs.Message, tryAgain bool, context *gin.Contex
 		// if failed and cannot try again,
 		fmt.Println("Append has failed, restart entire process later")
 		time.Sleep(time.Second * 30) // simulate pause
-		requestMasterAppend(message.Ports[0], message.Filename)
+		requestMasterAppend(message.Ports[0], message.SourceFilename, message.Filename)
 
 	} else { // success either case
 		fmt.Println("Append succeeded, proceed to confirm write")
@@ -175,17 +178,17 @@ func confirmWrite(message structs.Message, tryAgain bool, context *gin.Context) 
 		// if failed and cannot try again,
 		fmt.Println("Write has failed, restart entire process later")
 		time.Sleep(time.Second * 30) // simulate pause
-		requestMasterAppend(message.Ports[0], message.Filename)
+		requestMasterAppend(message.Ports[0], message.SourceFilename, message.Filename)
 
 	} else { // success either case
 		fmt.Println("Write succeeded, Client successfully appended")
 	}
 }
 
-func InitClient(id int, portNumber int) {
+func InitClient(id int, portNumber int, sourceFilename string, OFSFilename string) {
 	fmt.Printf("Client %d is going up at %d\n", id, portNumber)
 	go listen(id, portNumber)
-	requestMasterAppend(portNumber, "test.txt")
+	requestMasterAppend(portNumber, sourceFilename, OFSFilename)
 	for {
 
 	}
--- Structs/message.go
+++ Structs/message.go
@@ -3,14 +3,15 @@ package structs
 import "strconv"
 
 type Message struct {
-	MessageType string
-	Ports       []int  // 0: Client, 1: Primary, 2+: Secondary
-	Pointer     int    // initially 0, increment by 1 everytime received, decrement when ACK
-	Filename    string // filename which client requests to append
-	ChunkId     string
-	Payload     string
-	PayloadSize int64
-	ChunkOffset int64 // Offset at which the data is to be written
+	MessageType    string
+	Ports          []int // 0: Client, 1: Primary, 2+: Secondary
+	Pointer        int   // initially 0, increment by 1 everytime received, decrement when ACK
+	SourceFilename string
+	Filename       string // filename which client requests to append
+	ChunkId        string
+	Payload        string
+	PayloadSize    int64
+	ChunkOffset    int64 // Offset at which the data is to be written
 }
 
 func (message Message) GenerateUid() string {
@@ -18,27 +19,26 @@ func (message Message) GenerateUid() string {
 	return uid
 }
 
-func (message *Message) Forward()  {
+func (message *Message) Forward() {
 	message.Pointer += 1
 }
 
-func (message *Message) Reply()  {
+func (message *Message) Reply() {
 	message.Pointer -= 1
 }
 
-func (message *Message) SetMessageType(messageType string)  {
+func (message *Message) SetMessageType(messageType string) {
 	message.MessageType = messageType
 }
 
-func (message *Message) SetPorts(ports []int)  {
+func (message *Message) SetPorts(ports []int) {
 	message.Ports = ports
 }
 
-func (message *Message) SetPayload(payload string)  {
+func (message *Message) SetPayload(payload string) {
 	message.Payload = payload
 }
 
-func (message *Message) SetChunkOffset(offset int64)  {
+func (message *Message) SetChunkOffset(offset int64) {
 	message.ChunkOffset = offset
 }
-
