de47530e6c07d855e0f200ed5888cc4ad44a67fa
KiranM27 <kiran_bodhapati@mymail.sutd.edu.sg>
2022-04-10 01:07:19 +0800

    Landing page p much done

--- ui/src/components/AppLayout/AppLayout.js
+++ ui/src/components/AppLayout/AppLayout.js
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { Layout, Menu } from "antd";
-import { HomeOutlined, ReadOutlined, EditOutlined } from "@ant-design/icons";
+import { HomeOutlined, ReadOutlined, EditOutlined, FolderOpenOutlined } from "@ant-design/icons";
 import App from "../../App";
+import "./AppLayout.css"
 
-const { Sider, Footer, Content } = Layout;
+const { Sider, Header, Footer, Content } = Layout;
 
 export default function AppLayout() {
   const [collapsed, setCollapsed] = useState(false);
@@ -16,7 +17,7 @@ export default function AppLayout() {
     <>
       <Layout style={{ minHeight: "100vh" }}>
         <Sider collapsible collapsed={collapsed} onCollapse={onCollapse}>
-          <div className="logo" />
+          <div className="logo" style= {{color: "white", textAlign: "center"}}> <FolderOpenOutlined /> </div>
           <Menu theme="dark" defaultSelectedKeys={["1"]} mode="inline">
             <Menu.Item key="1" icon={<HomeOutlined />}>
               Home
@@ -30,6 +31,7 @@ export default function AppLayout() {
           </Menu>
         </Sider>
         <Layout className="site-layout">
+        <Header style={{ padding: 0, color: "white", textAlign: "center" }} >  Okay File System </Header>
         <Content style={{ margin: '0 16px' }}>
             <App />
           </Content>
--- ui/src/pages/LandingPage/LandingPage.js
+++ ui/src/pages/LandingPage/LandingPage.js
@@ -1,12 +1,53 @@
 import React from "react";
-import { Layout } from "antd";
+import "./LandingPage.css";
 
 export default function LandingPage() {
   return (
     <>
-      <Layout>
-        <div> Landing Page here </div>
-      </Layout>
+      <section class="section-a">
+        <div class="container">
+          <div>
+            <h2>The Okay File System </h2>
+            <p>
+              The Okay File System (OFS), inspired by the Google File System
+              (GFS), is a distributed file system that makes it conveninent to
+              share information and files among users. The scalable, highly
+              available and eventually consistent properties of the OFS allow
+              multiple users to perform any number of read and write operations
+              concurrently.
+            </p>
+            <div>
+              <h3>Features </h3>
+              <ul style={{ paddingLeft: "50px" }}>
+                <li>Atomic Append</li>
+                <li>Read</li>
+                <li>Chunking</li>
+                <li>Replication</li>
+                <li>Heartbeat</li>
+              </ul>
+            </div>
+            <a
+              href="https://github.com/KiranM27/OkayFileSystyem"
+              class="btn"
+              target="_blank"
+            >
+              OFS GitHub
+            </a>{" "}
+            &nbsp;
+            <a
+              href="https://static.googleusercontent.com/media/research.google.com/en//archive/gfs-sosp2003.pdf"
+              class="btn"
+              target="_blank"
+            >
+              GFS Paper
+            </a>
+          </div>
+          <img
+            src="https://blog.postman.com/wp-content/uploads/2021/03/Default-Blog-Image-1.png"
+            alt=""
+          />
+        </div>
+      </section>
     </>
   );
 }
